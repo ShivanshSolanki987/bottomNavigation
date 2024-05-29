07e620e141c528e256b31114e78fdce8bed6ce9b
Shivansh Solanki <shivanshsolanki9@gmail.com>
2024-05-30 00:24:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import home from './img/home.png';
+import phone from './img/phone.png';
+import wifi from './img/wifi.png';
+import loc from './img/location.png';
 
+function handleClick(e){
+  let arr = e.target.parentNode.childNodes;
+  for(let node of arr){
+    if(node!=e.target){
+      node.classList.remove("active");
+    }
+  }
+  e.target.classList.add("active");
+}
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <div className='active icon' onClick={(e)=>{handleClick(e)}}><div><img src={home}/></div></div>
+        <div className='icon' onClick={(e)=>{handleClick(e)}}><div><img src={phone}/></div></div>
+        <div className='icon' onClick={(e)=>{handleClick(e)}}><div><img src={wifi}/></div></div>
+        <div className='icon' onClick={(e)=>{handleClick(e)}}><div><img src={loc}/></div></div>
+      </div>
     </div>
   );
 }
